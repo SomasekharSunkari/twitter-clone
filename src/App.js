175bdd0ab9eab3e87671c39d4a95ba5f2e62708e
Sekhar <sunkarisekhar36@gmail.com>
2024-02-06 22:39:43 +0530

    Twitter-Clone started

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Sidebar from "./Components/Sidebar";
+
+function App() {
+  return (
+    <>
+      {/* Twitter Fullstack app clone creation */}
+      {/* Sideabar */}
+      <Sidebar />
+
+      {/* Tweets */}
+
+      {/* Widgets */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import { red } from "@mui/material/colors";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <TwitterIcon />
+    </div>
+  );
+};
+
+export default Sidebar;
