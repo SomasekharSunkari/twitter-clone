a2008c262b1fddad484f08a03c25f9f5a4a94295
Sekhar <sunkarisekhar36@gmail.com>
2024-02-09 11:15:18 +0530

    Just Started

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Sidebar from "./Components/Sidebar";
-
+import Sidebaroptions from "./Components/Sidebaroptions";
+import ExploreIcon from "@mui/icons-material/Explore";
+import HomeIcon from "@mui/icons-material/Home";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import LocalPostOfficeIcon from "@mui/icons-material/LocalPostOffice";
+import TextSnippetIcon from "@mui/icons-material/TextSnippet";
+import PersonIcon from "@mui/icons-material/Person";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 function App() {
   return (
     <>
       {/* Twitter Fullstack app clone creation */}
       {/* Sideabar */}
       <Sidebar />
-
+      <Sidebaroptions name="Home" Icon={HomeIcon} />
+      <Sidebaroptions name="Explore" Icon={ExploreIcon} />
+      <Sidebaroptions name="Notifications" Icon={NotificationsIcon} />
+      <Sidebaroptions name="Messages" Icon={LocalPostOfficeIcon} />
+      <Sidebaroptions name="Grok" Icon={TextSnippetIcon} />
+      <Sidebaroptions name="Profile" Icon={PersonIcon} />
+      <Sidebaroptions name="More.." Icon={MoreHorizIcon} />
       {/* Tweets */}
 
       {/* Widgets */}
---
+++ src/Components/Sidebaroptions.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Sidebaroptions = ({ name, Icon }) => {
+  return (
+    <div>
+      <Icon />
+      {name}
+    </div>
+  );
+};
+
+export default Sidebaroptions;
