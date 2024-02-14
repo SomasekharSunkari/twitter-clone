d9300aea322f0b82fcad24ed11fcc7f2f24dd284
Sekhar <sunkarisekhar36@gmail.com>
2024-02-14 09:16:41 +0530

    Small Changes Needed

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,9 +1,15 @@
 import React from "react";
+import ModeCommentOutlinedIcon from "@mui/icons-material/ModeCommentOutlined";
+import SettingsBackupRestoreOutlinedIcon from "@mui/icons-material/SettingsBackupRestoreOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import EqualizerOutlinedIcon from "@mui/icons-material/EqualizerOutlined";
+import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
+import IosShareOutlinedIcon from "@mui/icons-material/IosShareOutlined";
 
 const Item = ({ logo, AccoutName, Date, summary, img }) => {
   return (
     <>
-      <div className="text-white">
+      <div className="text-white ">
         <div class="flex w-[70%] mx-auto">
           <img
             src={logo}
@@ -20,6 +26,16 @@ const Item = ({ logo, AccoutName, Date, summary, img }) => {
         </div>
         <br />
         <img src={img} className="h-[450px] w-[70%] mx-auto" />
+        <div className="flex w-[70%] mx-auto pt-5">
+          <ModeCommentOutlinedIcon className="flex-1 cursor-pointer" />
+          <SettingsBackupRestoreOutlinedIcon className="flex-1 cursor-pointer" />
+          <FavoriteBorderOutlinedIcon className="flex-1 cursor-pointer" />
+          <EqualizerOutlinedIcon className="flex-1 cursor-pointer" />
+          <div className="flex-1 justify-self-end">
+            <BookmarkBorderOutlinedIcon className="cursor-pointer" />
+            <IosShareOutlinedIcon className="cursor-pointer" />
+          </div>
+        </div>
       </div>
       {/* Logo */}
       {/* Account name , date of posting*/}
